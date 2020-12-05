91700e2faaec23d267315bc88444c32f1ba298ea
Ville Naukkarinen <naukkis@gmail.com>
2020-12-05 16:20:41 +0200

    refine routes

--- src/main/kotlin/codes/naukkis/banksapi/services/NordeaAccountService.kt
+++ src/main/kotlin/codes/naukkis/banksapi/services/NordeaAccountService.kt
@@ -5,7 +5,7 @@ import codes.naukkis.banksapi.getHttpDate
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 import java.net.http.HttpRequest
@@ -19,12 +19,12 @@ class NordeaAccountService(private val config: Config) {
     var logger: Logger = Logger.getLogger(NordeaAccountService::class.java.name)
     private val httpClient = HttpClientProvider(config).noRedirectHttpClient
 
-    @GetMapping("/nordea/accounts", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/nordea/accounts/all", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun accounts(): String {
         val requestBuilder = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts"))
-                .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
+            .GET()
+            .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts"))
+            .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
 
         val request = setRegularHeaders(requestBuilder).build()
 
@@ -34,11 +34,11 @@ class NordeaAccountService(private val config: Config) {
     }
 
     @GetMapping("/nordea/accounts/{accountId}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun account(@RequestParam accountId: String): String {
+    fun account(@PathVariable accountId: String): String {
         val requestBuilder = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts/${accountId}"))
-                .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
+            .GET()
+            .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts/${accountId}"))
+            .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
 
         val request = setRegularHeaders(requestBuilder).build()
 
@@ -48,11 +48,11 @@ class NordeaAccountService(private val config: Config) {
     }
 
     @GetMapping("/nordea/accounts/{accountId}/transactions", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun transactions(@RequestParam accountId: String): String {
+    fun transactions(@PathVariable accountId: String): String {
         val requestBuilder = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts/${accountId}/transactions"))
-                .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
+            .GET()
+            .uri(URI.create("https://api.nordeaopenbanking.com/personal/v4/accounts/${accountId}/transactions"))
+            .setHeader("Authorization", "Bearer ${NordeaAuthController(config).getAccessToken().access_token}")
 
         val request = setRegularHeaders(requestBuilder).build()
 
@@ -64,10 +64,10 @@ class NordeaAccountService(private val config: Config) {
 
     private fun setRegularHeaders(builder: HttpRequest.Builder): HttpRequest.Builder {
         builder.setHeader("X-IBM-Client-ID", config.nordeaClientId)
-                .setHeader("X-IBM-Client-Secret", config.nordeaClientSecret)
-                .setHeader("X-Nordea-Originating-Date", getHttpDate())
-                .setHeader("X-Nordea-Originating-Host", "api.nordeaopenbanking.com")
-                .setHeader("Signature", "SKIP_SIGNATURE_VALIDATION_FOR_SANDBOX")
+            .setHeader("X-IBM-Client-Secret", config.nordeaClientSecret)
+            .setHeader("X-Nordea-Originating-Date", getHttpDate())
+            .setHeader("X-Nordea-Originating-Host", "api.nordeaopenbanking.com")
+            .setHeader("Signature", "SKIP_SIGNATURE_VALIDATION_FOR_SANDBOX")
         //      .setHeader("Digest", "sha-256=qKEkYt43vgJXW0ibKcHuvm+GhtsOKa/yISq9xk5pVV0=")
         return builder
     }
